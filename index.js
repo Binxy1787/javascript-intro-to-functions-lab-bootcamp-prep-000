1b35536a0c0770cee83d2d63477adffed7d748e7
Jameson Nelson <nelson_jameson@yahoo.com>
2019-01-24 21:52:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   
-  if(string.toLowerCase()){
+  if(string.toLowerCase() === 'hello'){
     return string;
-  }else if(string.toUpperCase()){
+  }else if(string.toUpperCase() === ''){
     return string;
   }else if(string === "I love you, Grandma."){
     return string;
